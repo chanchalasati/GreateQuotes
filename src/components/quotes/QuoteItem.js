a5818aa53ae10602da51bfd71c51b28b6f24e4aa
chanchalasati <chanchalasati12@gmail.com>
2021-11-01 23:26:33 +0530

    chanchalasati/GreateQuotes

--- src/components/quotes/QuoteItem.js
+++ src/components/quotes/QuoteItem.js
@@ -9,7 +9,7 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className='btn'>
+      <a>
         View Fullscreen
       </a>
     </li>
